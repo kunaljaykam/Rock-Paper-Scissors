61484cdd2c327e5bb5ce1840c90e320cb8e4fec8
kunal jaykam <kunaljaykam@icloud.com>
2021-06-13 02:51:23 +0530

    final

--- static/js/script.js
+++ static/js/script.js
@@ -10,7 +10,7 @@ function rpsGame(yourChoice) {
     console.log(results);
     message = finalMessage(results);
     console.log(message)
-   // rpsFrontEnd(yourChoice.id, botChoice, message);
+    rpsFrontEnd(yourChoice.id, botChoice, message);
 }
 
 function randomToRpsInt() {
@@ -48,12 +48,36 @@ function finalMessage([yourScore, computerScore]) {
 
 
 function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
-    let imagesDatbase = {
+    let imagesDatabase = {
         'rock': document.getElementById('rock').src,
         'paper': document.getElementById('paper').src,
         'scissors': document.getElementById('scissors').src,
     }
+// remove images
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+    
 
-    document.getElementById('')
-    //3:41
+
+    let humanDiv = document.createElement('div');
+    let botDiv = document.createElement('div');
+    let messageDiv = document.createElement('div');
+
+
+    humanDiv.innerHTML = "<img src'" + imagesDatabase[humanImageChoice] + "'height=150 width=150 style= 'box-shadow: 0px, 10px, 50px, rgba(255,255,255)>";
+
+    messageDiv.innerHTML = "<h2 style='color:  " + finalMessage['color'] +"; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h2>";
+    botDiv.innerHTML =
+         "<img src'" +
+         imagesDatabase[humanImageChoice] +
+         "'height=150 width=150 style= 'box-shadow: 0px, 10px, 50px, rgba(255,255,255)>";
+
+    document.getElementById('flex-box-rps-dev').appendChild(humanDiv);
+
+    document.getElementById('flex-box-rps-dev').appendChild(messageDiv);
+
+
+     document.getElementById("flex-box-rps-dev").appendChild(botDiv);
+
 }
